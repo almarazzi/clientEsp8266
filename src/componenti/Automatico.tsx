227f9de9e97a36a1954f72c21c9f672a7ef581b7
Marazzi Alberto <almarazzi05@gmail.com>
2025-04-05 17:34:48 +0200

    Tolti dei bug

--- src/componenti/Automatico.tsx
+++ src/componenti/Automatico.tsx
@@ -37,21 +37,11 @@ export function Automatico (props:{mac:string}){
       <Fragment>
         <div className="Manuale1">
             <button type="button" className={ "Buttone1  btn btn-" + (state === true ?  "primary" : "secondary") }  onClick={async ()=>{ 
-              let data = await fetch("/api/RelaySwitch/SetState", {  method: "PUT", body: JSON.stringify({state:true, macricever:props.mac }) ,headers: { 'Content-type': 'application/json; charl set=UTF-8' }});
-              var res = await data.json() as Tutto[];
-              res.map((y,_)=>{
-                if(y.macricever===props.mac)
-                stateOn(y.state);
-                })
+              await fetch("/api/RelaySwitch/SetState", {  method: "PUT", body: JSON.stringify({state:true, macricever:props.mac }) ,headers: { 'Content-type': 'application/json; charl set=UTF-8' }});
             }}> ON</button>
 
             <button type="button" className={ "Buttone2 btn btn-" + (state === false ?  "primary" : "secondary") }  onClick={async ()=>{ 
-              let data = await fetch("/api/RelaySwitch/SetState" , { method: "PUT",body: JSON.stringify({state:false, macricever:props.mac }), headers: { 'Content-type': 'application/json; charl set=UTF-8' }});
-              var res = await data.json() as Tutto[];
-              res.map((y,_)=>{
-                if(y.macricever===props.mac)
-                stateOn(y.state);
-                })
+              await fetch("/api/RelaySwitch/SetState" , { method: "PUT",body: JSON.stringify({state:false, macricever:props.mac }), headers: { 'Content-type': 'application/json; charl set=UTF-8' }});
             }}> OFF</button>
         </div>
         </Fragment>
